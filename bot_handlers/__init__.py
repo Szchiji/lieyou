1499030e9be6358f0d990cca47a5bd536ef06ed5
Szchiji <118823636+Szchiji@users.noreply.github.com>
2025-09-08 23:39:04 +0800

    Update __init__.py

--- bot_handlers/__init__.py
+++ bot_handlers/__init__.py
@@ -1,50 +1,105 @@
-from .start import start
-from .common import check_if_user_is_member, cancel_action
-from .menu import (
-    show_private_main_menu, 
-    private_menu_callback_handler,
-    AVAILABLE_ACTIONS
-)
-from .reputation import (
-    handle_query, 
-    reputation_callback_handler,
-    tag_callback_handler
-)
-from .leaderboard import (
-    show_leaderboard_callback_handler,
-    leaderboard_type_callback_handler
+"""This package contains all the bot's callback handlers."""
+
+from .start import (
+    start,
+    show_private_main_menu
 )
 from .admin import (
     admin_panel,
     manage_tags_panel,
+    delete_tag_callback,
     add_tag_prompt,
     handle_new_tag,
     handle_tag_type_selection,
-    delete_tag_callback,
     manage_menu_buttons_panel,
+    delete_menu_button_callback,
+    toggle_menu_button_callback,
+    reorder_menu_button_callback,
     add_menu_button_prompt,
     handle_new_menu_button_name,
     handle_new_menu_button_action,
-    reorder_menu_button_callback,
-    toggle_menu_button_callback,
-    delete_menu_button_callback,
     user_management_panel,
     prompt_for_username,
     set_user_hidden_status,
+    prompt_for_broadcast,
+    get_broadcast_content,
+    confirm_broadcast,
+    cancel_action,
+    # Conversation states
     TYPING_TAG_NAME,
     SELECTING_TAG_TYPE,
     TYPING_BUTTON_NAME,
     SELECTING_BUTTON_ACTION,
     TYPING_USERNAME_TO_HIDE,
-    TYPING_USERNAME_TO_UNHIDE
-)
-from .broadcast import (
-    prompt_for_broadcast,
-    get_broadcast_content,
-    confirm_broadcast,
-    send_broadcast,
+    TYPING_USERNAME_TO_UNHIDE,
     TYPING_BROADCAST,
-    CONFIRM_BROADCAST
+    CONFIRM_BROADCAST,
 )
-from .report import generate_my_report
-from .monitoring import run_suspicion_monitor
+from .reputation import (
+    reputation_callback_handler,
+    handle_query
+)
+from .tags import (
+    tag_callback_handler
+)
+from .menu import (
+    private_menu_callback_handler,
+    show_leaderboard_callback_handler,
+    leaderboard_type_callback_handler
+)
+from .reports import (
+    generate_my_report
+)
+from .monitoring import (
+    run_suspicion_monitor
+)
+
+
+__all__ = [
+    # start
+    'start',
+    'show_private_main_menu',
+    # admin
+    'admin_panel',
+    'manage_tags_panel',
+    'delete_tag_callback',
+    'add_tag_prompt',
+    'handle_new_tag',
+    'handle_tag_type_selection',
+    'manage_menu_buttons_panel',
+    'delete_menu_button_callback',
+    'toggle_menu_button_callback',
+    'reorder_menu_button_callback',
+    'add_menu_button_prompt',
+    'handle_new_menu_button_name',
+    'handle_new_menu_button_action',
+    'user_management_panel',
+    'prompt_for_username',
+    'set_user_hidden_status',
+    'prompt_for_broadcast',
+    'get_broadcast_content',
+    'confirm_broadcast',
+    'cancel_action',
+    # reputation
+    'reputation_callback_handler',
+    'handle_query',
+    # tags
+    'tag_callback_handler',
+    # menu
+    'private_menu_callback_handler',
+    'show_leaderboard_callback_handler',
+    'leaderboard_type_callback_handler',
+    # reports
+    'generate_my_report',
+    # monitoring
+    'run_suspicion_monitor',
+    # Conversation states
+    'TYPING_TAG_NAME',
+    'SELECTING_TAG_TYPE',
+    'TYPING_BUTTON_NAME',
+    'SELECTING_BUTTON_ACTION',
+    'TYPING_USERNAME_TO_HIDE',
+    'TYPING_USERNAME_TO_UNHIDE',
+    'TYPING_BROADCAST',
+    'CONFIRM_BROADCAST',
+]
