f08498b2828d3a111da344e03642a623edbce07b
Szchiji <118823636+Szchiji@users.noreply.github.com>
2025-09-09 01:06:02 +0800

    Update __init__.py

--- bot_handlers/__init__.py
+++ bot_handlers/__init__.py
@@ -1,105 +1,30 @@
-"""This package contains all the bot's callback handlers."""
+"""
+This file makes the bot_handlers directory a Python package.
+It also serves as a central point for exporting all the handlers
+so they can be easily imported in main.py.
+"""
 
 from .start import (
     start,
-    show_private_main_menu
+    show_private_main_menu,
 )
+
+from .reputation import (
+    handle_query,
+    evaluation_callback_handler,
+)
+
 from .admin import (
     admin_panel,
-    manage_tags_panel,
-    delete_tag_callback,
-    add_tag_prompt,
-    handle_new_tag,
-    handle_tag_type_selection,
-    manage_menu_buttons_panel,
-    delete_menu_button_callback,
-    toggle_menu_button_callback,
-    reorder_menu_button_callback,
-    add_menu_button_prompt,
-    handle_new_menu_button_name,
-    handle_new_menu_button_action,
-    user_management_panel,
-    prompt_for_username,
-    set_user_hidden_status,
-    prompt_for_broadcast,
-    get_broadcast_content,
-    confirm_broadcast,
-    cancel_action,
-    # Conversation states
-    TYPING_TAG_NAME,
-    SELECTING_TAG_TYPE,
-    TYPING_BUTTON_NAME,
-    SELECTING_BUTTON_ACTION,
-    TYPING_USERNAME_TO_HIDE,
-    TYPING_USERNAME_TO_UNHIDE,
-    TYPING_BROADCAST,
-    CONFIRM_BROADCAST,
-)
-from .reputation import (
-    reputation_callback_handler,
-    handle_query
-)
-from .tags import (
-    tag_callback_handler
-)
-from .menu import (
-    private_menu_callback_handler,
-    show_leaderboard_callback_handler,
-    leaderboard_type_callback_handler
-)
-from .reports import (
-    generate_my_report
-)
-from .monitoring import (
-    run_suspicion_monitor
+    # Add other admin functions here as they are built
 )
 
-
+# This makes it possible to use "from bot_handlers import *"
+# although explicit imports are generally better.
 __all__ = [
-    # start
     'start',
     'show_private_main_menu',
-    # admin
-    'admin_panel',
-    'manage_tags_panel',
-    'delete_tag_callback',
-    'add_tag_prompt',
-    'handle_new_tag',
-    'handle_tag_type_selection',
-    'manage_menu_buttons_panel',
-    'delete_menu_button_callback',
-    'toggle_menu_button_callback',
-    'reorder_menu_button_callback',
-    'add_menu_button_prompt',
-    'handle_new_menu_button_name',
-    'handle_new_menu_button_action',
-    'user_management_panel',
-    'prompt_for_username',
-    'set_user_hidden_status',
-    'prompt_for_broadcast',
-    'get_broadcast_content',
-    'confirm_broadcast',
-    'cancel_action',
-    # reputation
-    'reputation_callback_handler',
     'handle_query',
-    # tags
-    'tag_callback_handler',
-    # menu
-    'private_menu_callback_handler',
-    'show_leaderboard_callback_handler',
-    'leaderboard_type_callback_handler',
-    # reports
-    'generate_my_report',
-    # monitoring
-    'run_suspicion_monitor',
-    # Conversation states
-    'TYPING_TAG_NAME',
-    'SELECTING_TAG_TYPE',
-    'TYPING_BUTTON_NAME',
-    'SELECTING_BUTTON_ACTION',
-    'TYPING_USERNAME_TO_HIDE',
-    'TYPING_USERNAME_TO_UNHIDE',
-    'TYPING_BROADCAST',
-    'CONFIRM_BROADCAST',
+    'evaluation_callback_handler',
+    'admin_panel',
 ]
